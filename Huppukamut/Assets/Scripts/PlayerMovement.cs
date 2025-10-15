232e8c0f314d26d0cb4073d3d93cd52f86b6795d
Rantsu\Devaus <eemi.pekka1995@gmail.com>
2025-10-15 12:02:15 +0300

    Main menu ja level scenet lisätty, myös oma test scene.

---
+++ Huppukamut/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float movementHorizontal = Input.GetAxis("Horizontal");
+        float movementVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movementHorizontal, 0.0f, movementVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}

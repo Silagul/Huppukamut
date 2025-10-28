af5aeaf8f6bc216494d36d6353aa012f34aec2f7
Rantsu\Devaus <olli.roihupalo@gmail.com>
2025-10-28 14:05:59 +0200

    Smoothened main menu camera movement. Started working on character select scene.

---
+++ Huppukamut/Assets/Scripts/Menus/CharacterSelectController.cs
@@ -0,0 +1,97 @@
+using System;
+using UnityEngine;
+
+public class CharacterSelectController : MonoBehaviour
+{
+    public GameObject platform;
+    public GameObject[] characters;
+    public TMPro.TextMeshProUGUI nameDisplay;
+    public TMPro.TextMeshProUGUI description;
+    public string[,] characterData;
+    public int current = 0;
+    public bool clockwise;
+    public bool spinning = false;
+    public float rotationTime;
+    private float rotationTimer;
+    private float step;
+    private float speed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        platform = GameObject.Find("Platform");
+        characters = GameObject.FindGameObjectsWithTag("Player");
+        nameDisplay = GameObject.Find("CharName").GetComponent<TMPro.TextMeshProUGUI>();
+        description = GameObject.Find("CharDescription").GetComponent<TMPro.TextMeshProUGUI>();
+        step = 360 / characters.Length;
+        rotationTimer = 0f;
+        speed = step / rotationTime;
+        characterData = new string[characters.Length, 2];
+        for (int i = 0; i < characters.Length; i++)
+        {
+            characterData[i, 0] = characters[i].name;
+            characterData[i, 1] = characters[i].name + " description";
+        }
+        nameDisplay.text = characterData[0, 0];
+
+        //characterData = { { "Character 1 name", "Character 1 description" }, { "Character 2 name", "Character 2 description" }, { "Character 3 name", "Character 3 description" }, { "Character 4 name", "Character 4 description" } };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nameDisplay.text = characterData[current, 0]; 
+        
+        rotationTimer -= Time.deltaTime;
+        if (rotationTimer <= 0f)
+        {
+            spinning = false;
+        }
+        if (spinning)
+        {
+            foreach (GameObject character in characters)
+            {
+                if (clockwise)
+                {
+                    character.transform.RotateAround(platform.transform.position, Vector3.up, speed * Time.deltaTime);
+                }
+                else
+                {
+                    character.transform.RotateAround(platform.transform.position, Vector3.up, -1 * speed * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    public void RotateSelection(bool cw)
+    {
+        if (!spinning)
+        {
+            spinning = true;
+            clockwise = cw;
+            rotationTimer = rotationTime;
+            if (cw)
+            {
+                if (current == 0)
+                {
+                    current = characters.Length - 1;
+                }
+                else
+                {
+                    current--;
+                }
+            }
+            else
+            {
+                if (current == characters.Length - 1)
+                {
+                    current = 0;
+                }
+                else
+                {
+                    current++;
+                }
+            }
+        }
+    }
+}
---
+++ Huppukamut/Assets/Scripts/Menus/MainMenuController.cs
@@ -0,0 +1,91 @@
+using TMPro;
+using UnityEditor;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Transform cam;
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject creditsMenu;
+    public GameObject linksMenu;
+    public GameObject current;
+    public Transform target;
+    public float moveStep;
+    public float rotateStep;
+
+    private Quaternion targetRotation = new Quaternion(0, 0, 0, 1);
+    private float ang = 0f;
+    private float distance = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        cam = Camera.main.transform;
+        mainMenu = GameObject.Find("MainMenu");
+        settingsMenu = GameObject.Find("SettingsMenu");
+        creditsMenu = GameObject.Find("CreditsMenu");
+        linksMenu = GameObject.Find("LinksMenu");
+
+        GameObject[] menus = { mainMenu, settingsMenu, creditsMenu, linksMenu };
+        foreach (GameObject menu in menus)
+        {
+            menu.transform.Find("Canvas").gameObject.SetActive(false);
+        }
+        current = mainMenu;
+        target = current.transform.Find("CameraTarget");
+        ChangeActiveMenu(current);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move our position a step closer to the target.
+        Vector3 targetDirection = current.transform.position - cam.position;
+        //float step = moveStep * Time.deltaTime; // calculate distance to move
+        float step = (distance + 10) * Time.deltaTime; // calculate distance to move
+        cam.position = Vector3.MoveTowards(cam.position, target.position, step);
+
+        //float ang2 = Quaternion.Angle(cam.rotation, current.transform.rotation);
+        float singleStep = rotateStep * Time.deltaTime;
+        if (current == mainMenu)
+        {
+            if (targetDirection.sqrMagnitude <= 400)
+            {
+                Vector3 newDirection = Vector3.RotateTowards(cam.forward, targetDirection, singleStep * 1.5f, 0.0f);
+                cam.rotation = Quaternion.LookRotation(newDirection);
+            }
+        }
+        else
+        {
+            Vector3 newDirection = Vector3.RotateTowards(cam.forward, targetDirection, singleStep, 0.0f);
+            cam.rotation = Quaternion.LookRotation(newDirection);
+        }
+    }
+
+    public void ChangeActiveMenu(GameObject menu)
+    {
+        GameObject[] menus = { mainMenu, settingsMenu, creditsMenu, linksMenu };
+        foreach (GameObject m in menus)
+        {
+            if (m != menu)
+            {
+                m.transform.Find("Canvas").gameObject.SetActive(false);
+            }
+            else
+            {
+                m.transform.Find("Canvas").gameObject.SetActive(true);
+            }
+        }
+        ang = Quaternion.Angle(cam.rotation, menu.transform.rotation);
+        current = menu;
+        target = menu.transform.Find("CameraTarget");
+        Vector3 targetDirection = target.transform.position - cam.position;
+        distance = targetDirection.magnitude;
+
+        //print(ang + " degrees");
+        //print(distance + " units length");
+    }
+}

23fe733e0eec16a35849fc34bc3ff66e7008f867
Rantsu\Devaus <mflyktman05@gmail.com>
2025-11-04 10:29:09 +0200

    added the emote test

---
+++ Huppukamut/Assets/Scenes/Mika_EmoteTest/ImageCharger3.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIImageChanger : MonoBehaviour
+{
+    public Sprite image1;
+    public Sprite image2;
+    public Sprite image3;
+
+    private Image uiImage;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        uiImage = GetComponent<Image>();
+        uiImage.sprite = image1;
+    }
+
+    public void ChangeImage()
+    {
+        currentIndex++;
+
+        if (currentIndex == 1)
+            uiImage.sprite = image2;
+        else if (currentIndex == 2)
+            uiImage.sprite = image3;
+        else
+        {
+            uiImage.sprite = image1;
+            currentIndex = 0;
+        }
+    }
+}

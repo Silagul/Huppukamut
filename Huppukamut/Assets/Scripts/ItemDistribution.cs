29851ebf3243caef2793c1beeb6682b88fac0165
Rantsu\Devaus <olli.roihupalo@gmail.com>
2025-11-14 13:58:39 +0200

    Edited prefabs again. Made first version of item distribution.

--- Huppukamut/Assets/Scripts/ItemDistribution.cs
+++ Huppukamut/Assets/Scripts/ItemDistribution.cs
@@ -7,27 +7,73 @@ public class ItemDistribution : MonoBehaviour
     public GameObject[] itemPositions;
     public float budget;
 
+    private GameObject[] items = null;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         itemPositions = GameObject.FindGameObjectsWithTag("ItemPosition");
         float maxValuePerItem = budget / itemPositions.Length;
-
-        for (float i = budget; i > 0;)
+        float totalValue = 0;
+        
+        int a = 0;
+        foreach (GameObject item in itemPrefabs)
         {
-            int z = (int)Mathf.Ceil(Random.Range(0.1f, 7f));
-            Item b = itemPrefabs[z].GetComponent<Item>();
-            if (i >= b.budgetUsage && b.budgetUsage <= maxValuePerItem)
+            float b = item.GetComponent<Item>().budgetUsage;
+            if (b < maxValuePerItem)
             {
-                //Instantiate(itemPrefabs[z], itemPositions[?].transform.position, Quaternion.identity);
-                i -= b.budgetUsage;
-                break;
-            }
-            else
-            {
-                continue;
+                a++;
             }
         }
+
+        items = new GameObject[a];
+        int c = 0;
+        foreach (GameObject item in itemPrefabs)
+        {
+            float b = item.GetComponent<Item>().budgetUsage;
+            if (b < maxValuePerItem)
+            {
+                items[c] = item;
+                c++;
+            }
+        }
+
+        /*
+        GameObject holder = null;
+        float largest = 0;
+
+        foreach (GameObject item in items)
+        {
+            float b = item.GetComponent<Item>().budgetUsage;
+            if (b > largest)
+            {
+                holder = item;
+                largest = b;
+            }
+        }
+        float x = budget / largest;
+        */
+
+        for (int p = 0; p < itemPositions.Length; p++)
+        {
+            for (float i = budget; i > 0;)
+            {
+                int z = (int)Mathf.Ceil(Random.Range(0.1f, items.Length - 1));
+                Item b = items[z].GetComponent<Item>();
+                if (i >= b.budgetUsage)
+                {
+                    Instantiate(items[z], itemPositions[p].transform.position, Quaternion.identity);
+                    i -= b.budgetUsage;
+                    totalValue += b.budgetUsage;
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+        print("Leftover budget: " + (budget - totalValue));
     }
 
     // Update is called once per frame

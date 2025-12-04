9a6654b130c16ccb6fb59443338ece5ab1b56575
Rantsu\Devaus <mflyktman05@gmail.com>
2025-12-04 09:38:16 +0200

    Lisätty Musicplayer + Scripti siihen

--- Huppukamut/Assets/Scripts/DoNotDestroy.cs
+++ Huppukamut/Assets/Scripts/DoNotDestroy.cs
@@ -7,7 +7,8 @@ public class DoNotDestroy : MonoBehaviour
         GameObject[] musicObj = GameObject.FindGameObjectsWithTag("GameMusic");
         if(musicObj.Length > 1)
         {
-
+            Destroy(this.gameObject);
         }
+        DontDestroyOnLoad(this.gameObject);
     }
 }
---
+++ Huppukamut/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private static MusicPlayer instance;
+    private AudioSource audioSource;
+
+    [SerializeField] private string[] allowedScenes = { "MainMenu", "SelectCharacter" };
+    [SerializeField] private string stopScene = "Level1_copy";
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+
+        if (IsSceneAllowed(SceneManager.GetActiveScene().name))
+            audioSource.Play();
+
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+    }
+
+    void OnActiveSceneChanged(Scene oldScene, Scene newScene)
+    {
+        if (newScene.name == stopScene)
+        {
+            audioSource.Stop();
+            Destroy(gameObject);
+        }
+        else if (IsSceneAllowed(newScene.name) && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    bool IsSceneAllowed(string sceneName)
+    {
+        foreach (string s in allowedScenes)
+        {
+            if (s == sceneName)
+                return true;
+        }
+        return false;
+    }
+
+    // 🔹 Uusi metodi, joka hiljentää musiikin
+    public void MuteMusic()
+    {
+        if (audioSource != null)
+            audioSource.Stop();
+    }
+}
